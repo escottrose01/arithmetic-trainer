d5161644580bf493958a2797efd55b7fce75becd
Evan Rose <escottrose01@gmail.com>
2020-05-02 01:59:38 -0400

    Create Expression.java

---
+++ src/main/data/expression/Expression.java
@@ -0,0 +1,47 @@
+package main.data.expression;
+
+import main.records.Operator;
+
+public class Expression {
+    Expression left;
+    Expression right;
+    Operator operator;
+    Integer value;
+
+    public Expression(Expression left, Expression right, Operator operator) {
+        this.left = left;
+        this.right = right;
+        this.operator = operator;
+        value = null;
+    }
+
+    public Expression(int value) {
+        this.left = null;
+        this.right = null;
+        this.operator = null;
+        this.value = value;
+    }
+
+    public int evaluate() {
+        if (value != null) {
+            return value;
+        }
+
+        switch (operator) {
+            case ADD:
+                value = left.evaluate() + right.evaluate();
+                break;
+            case SUBTRACT:
+                value = left.evaluate() - right.evaluate();
+                break;
+            case MULTIPLY:
+                value = left.evaluate() * right.evaluate();
+                break;
+            case DIVIDE:
+                value = left.evaluate() / right.evaluate();
+                break;
+        }
+        
+        return value;
+    }
+}
